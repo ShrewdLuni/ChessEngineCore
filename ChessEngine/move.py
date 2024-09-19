2a8326f7dc1fba1945686252dc12e17b80c7ccd2
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-09-19 18:48:21 +0300

    refactor: new move structure move is now 16bits int, and have a flags

--- ChessEngine/move.py
+++ ChessEngine/move.py
@@ -1,4 +1,12 @@
 class Move:
-    def __init__(self, starting_square, target_square):
-        self.starting_square = starting_square
-        self.target_square = target_square
+    def __init__(self, starting_square, target_square, flag=0):
+        self.move = (flag << 12) | (target_square << 6) | starting_square
+
+    def get_starting_square(self):
+        return self.move & 0x3F
+
+    def get_target_square(self):
+        return (self.move >> 6) & 0x3F
+
+    def get_move_flag(self):
+        return (self.move >> 12) & 0xF
---
+++ ChessEngine/move_flags.py
@@ -0,0 +1,9 @@
+normal_move = 0
+en_passant_capture = 1
+castling = 2
+promote_to_queen = 3
+promote_to_knight = 4
+promote_to_rook = 5
+promote_to_bishop = 6
+pawn_two_forward = 7
+
