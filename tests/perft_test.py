13bc244c96f2c9e53ee33416989ab937e4f6d96f
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2025-03-07 12:47:53 +0100

    feat: test parametrize

--- tests/perft_test.py
+++ tests/perft_test.py
@@ -1,6 +1,20 @@
+import pytest
+
 from ChessEngine import Engine
 
-def test_perfit(depth: int = 5):
+
+@pytest.mark.parametrize("depth,expected", [
+    (1, 20),
+    (2, 400),
+    (3, 8902),
+    (4, 197281),
+    (5, 4865609),
+    # (6, 119060324),
+    # (7, 3195901860),
+    # (8, 84998978956),
+    # (9, 2439530234167),
+])
+def test_perfit(depth: int, expected: int):
     engine = Engine()
     result = engine.move_generation_test(depth, True)
-    assert result["count"] == 4865609
+    assert result["count"] == expected 
