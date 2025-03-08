aaa20e108614d53bb10469bdd1472e3bbcda75c1
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2025-03-08 14:11:16 +0100

    refactor: less literals

--- tests/perft_test.py
+++ tests/perft_test.py
@@ -2,21 +2,25 @@ import pytest
 
 from ChessEngine import Engine
 
+STARTPOS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+KIWIPETE = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
+
+
 @pytest.fixture
 def engine():
     return Engine()
 
 @pytest.mark.parametrize("position,depth,expected", [
-    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 1, 20),
-    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 2, 400),
-    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 3, 8902),
-    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 4, 197281),
-    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 5, 4865609),
+    (STARTPOS, 1, 20),
+    (STARTPOS, 2, 400),
+    (STARTPOS, 3, 8902),
+    (STARTPOS, 4, 197281),
+    (STARTPOS, 5, 4865609),
 
-    ("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 1, 48),
-    ("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 2, 2039),
-    ("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 3, 97862),
-    ("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4, 4085603),
+    (KIWIPETE, 1, 48),
+    (KIWIPETE, 2, 2039),
+    (KIWIPETE, 3, 97862),
+    (KIWIPETE, 4, 4085603),
 ])
 def test_perfit(engine: Engine, position: str, depth: int, expected: int):
     engine = Engine()
