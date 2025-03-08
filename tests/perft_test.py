abefaa38143fea0307f85e00c8281d5c544bba2f
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2025-03-08 13:59:56 +0100

    feat: add position param to tests

--- tests/perft_test.py
+++ tests/perft_test.py
@@ -3,18 +3,20 @@ import pytest
 from ChessEngine import Engine
 
 
-@pytest.mark.parametrize("depth,expected", [
-    (1, 20),
-    (2, 400),
-    (3, 8902),
-    (4, 197281),
-    (5, 4865609),
-    # (6, 119060324),
-    # (7, 3195901860),
-    # (8, 84998978956),
-    # (9, 2439530234167),
+@pytest.mark.parametrize("position,depth,expected", [
+    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 1, 20),
+    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 2, 400),
+    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 3, 8902),
+    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 4, 197281),
+    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 5, 4865609),
+
+    ("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 1, 48),
+    ("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 2, 2039),
+    ("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 3, 97862),
+    ("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4, 4085603),
 ])
-def test_perfit(depth: int, expected: int):
+def test_perfit(position: str, depth: int, expected: int):
     engine = Engine()
+    engine.set_position(position)
     result = engine.move_generation_test(depth, True)
     assert result["count"] == expected 
